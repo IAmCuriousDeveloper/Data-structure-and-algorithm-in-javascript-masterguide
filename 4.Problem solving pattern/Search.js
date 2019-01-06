249e1eba0da1639e83ff4bf408e54d0ed10a8f57
Prashant Rawal <prashantrawal619@gmail.com>
2019-01-06 23:16:52 +0530

    sliding window and divide and conquer added

---
+++ 4.Problem solving pattern/Search.js
@@ -0,0 +1,44 @@
+//Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1
+search([1,2,3,4,5,6],4) // 3
+search([1,2,3,4,5,6],6) // 5
+search([1,2,3,4,5,6],11) // -1
+
+//naive solution
+function search(arr, val){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === val){
+            return i;
+        }
+    }
+    return -1;
+}
+
+//Time Complexity O(N)
+
+//see the binary serach approach
+function search(array, val) {
+ 
+    let min = 0;
+    let max = array.length - 1;
+ 
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = array[middle];
+ 
+        if (array[middle] < val) {
+            min = middle + 1;
+        }
+        else if (array[middle] > val) {
+            max = middle - 1;
+        }
+        else {
+            return middle;
+        }
+    }
+ 
+    return -1;
+}
+
+//note binary search only applicable for the sorted array.
+//Time Complexity - Log(N)
+
