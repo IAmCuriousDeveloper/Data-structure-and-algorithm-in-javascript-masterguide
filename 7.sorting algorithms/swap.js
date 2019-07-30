ec9d955a5a8eb64933b3ae500cb6c7b017febbba
Prashant rawal <Prashantrawal619@gmail.com>
2019-07-30 10:12:50 +0530

    comment fixed

--- 7.sorting algorithms/swap.js
+++ 7.sorting algorithms/swap.js
@@ -1,12 +1,11 @@
 // ES5
 function swap(arr, idx1, idx2) {
-    var temp = arr[idx1];
-    arr[idx1] = arr[idx2];
-    arr[idx2] = temp;
-  }
-  
-  // ES2015
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1],arr[idx2]] = [arr[idx2],arr[idx1]];
-  }
-  
+  var temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+}
+
+// ES2015 es6
+const swap = (arr, idx1, idx2) => {
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+};
